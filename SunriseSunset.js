db9fd4d3f32b1368ddceffeaf244042d9c4da0a9
clairegaza <cgaza@villanova.edu>
2024-12-09 19:56:43 -0500

    changes rise and set file

--- SunriseSunset.js
+++ SunriseSunset.js
@@ -10,26 +10,16 @@ const SunriseSunset = ({route}) => {
     const tempInFahrenheit = Math.round(((weather.main.temp - 273.15) * 9) / 5 + 32);
     const [temperature, setTemperature] = useState(tempInFahrenheit);
 
-    let outfitSuggestion = '';
-    if (temperature > 75) {
-        outfitSuggestion = 'Wear light clothing like shorts and a t-shirt.';
-    } else if (temperature > 50) {
-        outfitSuggestion = 'Consider wearing a jacket and pants.';
-    } else {
-        outfitSuggestion = 'Dress warmly with a coat, scarf, and gloves.';
-    }
-
-    let outfitSuggestion2 = '';
-    if (weather.weather[0].main == "Rain")
-        outfitSuggestion2 = 'It is raining! Make sure you are wearing a raincoat!';
-
+    const sunriseTime = weather.sys.sunrise;
+    const sunsetTime = weather.sys.sunset;
     
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Welcome to the "Sunrise Sunset" page!</Text>
             <Text>Temperature: {temperature}°F</Text>
-            <Text>Outfit Suggestion: {outfitSuggestion} {outfitSuggestion2}</Text>
-
+            <Text>Sunrise: {sunriseTime}</Text>
+            <Text>Sunset: {sunsetTime}</Text>
+            <Text></Text>
         </View>
     );
 };
